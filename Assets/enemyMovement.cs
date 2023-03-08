c3dbcdfdfbdce28c3d105a894b1b467af9c4b697
NachoXS15 <86374911+NachoXS15@users.noreply.github.com>
2023-03-08 10:58:54 -0300

    added enemy spawns

--- Assets/enemyMovement.cs
+++ Assets/enemyMovement.cs
@@ -17,6 +17,7 @@ public class enemyMovement : MonoBehaviour
 
     // Update is called once per frame
     private void FixedUpdate(){
+        target = GameObject.FindGameObjectWithTag("Player").transform;
         Vector3 a = transform.position;
         Vector3 b = target.position;
         transform.position = Vector3.MoveTowards(a, b, speed);
--- Assets/spawnEnemy.cs
+++ Assets/spawnEnemy.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class spawnEnemy : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class spawnEnemy : MonoBehaviour {
+    
+    [SerializeField] private float spawnRate = 1f;
+    [SerializeField] private GameObject[] enemyPrefabs;
+    [SerializeField] private bool canSpawn = true;
+
+    private void Start() {
+        StartCoroutine(Spawner());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private IEnumerator Spawner() {
+        WaitForSeconds wait = new WaitForSeconds(spawnRate);
+        while (canSpawn){
+            yield return wait;
+            int random = Random.Range(0, enemyPrefabs.Length);
+            GameObject enemyToSpawn =  enemyPrefabs[random];
+            Instantiate(enemyToSpawn);
+        }
     }
 }
